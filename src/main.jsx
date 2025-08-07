48859eaadbd81a884a3b64e31ac94f9019d1d703
AdarshBaddies <adarshnothing578@gmail.com>
2025-08-07 23:33:24 +0530

    may be atlastdsfgjd

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,10 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-import { HashRouter } from 'react-router-dom';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter  basename="/collegewise/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
-);
-
+)
